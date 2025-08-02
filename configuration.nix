0b8f8de90a7295af4055f226074b1de18ee93e39
Mrid22 <mridulaga@outlook.com>
2025-08-03 00:33:30 +0200

    added fonts

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,13 @@
     blueberry
   ];
 
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    nerd-fonts.fira-code
+  ];
+
   home-manager = {
     extraSpecialArgs = {inherit inputs;};
     users = {
