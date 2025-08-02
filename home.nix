5d915c39f1aeb05d25a3e02ddc0bd55493da1825
Mrid22 <mridulaga@outlook.com>
2025-08-03 01:11:23 +0200

    formatting

--- home.nix
+++ home.nix
@@ -17,109 +17,91 @@
     packages = with pkgs; [
       spotdl
     ];
+
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
+
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
   };
 
-  programs.nvf = {
-    enable = true;
-    settings = {
-      vim = {
-        viAlias = true;
-        vimAlias = true;
-        lineNumberMode = "none";
-        withPython3 = true;
-        withNodeJs = true;
-        options = {
-          shiftwidth = 2;
-          tabstop = 2;
-        };
-        theme = {
-          enable = true;
-          name = "tokyonight";
-          style = "storm";
-          transparent = true;
-        };
-        statusline.lualine.enable = true;
-        telescope.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        assistant = {
-          codecompanion-nvim.enable = true;
-          copilot = {
-            enable = true;
-            cmp.enable = true;
+  programs = {
+    nvf = {
+      enable = true;
+      settings = {
+        vim = {
+          viAlias = true;
+          vimAlias = true;
+          lineNumberMode = "none";
+          withPython3 = true;
+          withNodeJs = true;
+          options = {
+            shiftwidth = 2;
+            tabstop = 2;
           };
-        };
-        languages = {
-          enableFormat = true;
-          enableTreesitter = true;
-          enableExtraDiagnostics = true;
-          enableDAP = true;
+          theme = {
+            enable = true;
+            name = "tokyonight";
+            style = "storm";
+            transparent = true;
+          };
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          autocomplete.nvim-cmp.enable = true;
+          assistant = {
+            codecompanion-nvim.enable = true;
+            copilot = {
+              enable = true;
+              cmp.enable = true;
+            };
+          };
+          languages = {
+            enableFormat = true;
+            enableTreesitter = true;
+            enableExtraDiagnostics = true;
+            enableDAP = true;
 
-          # Languages
-          nix.enable = true;
-          python.enable = true;
-          html.enable = true;
-          css.enable = true;
-          ts.enable = true;
-          markdown = {
-            enable = true;
-            extensions.markview-nvim.enable = true;
+            # Languages
+            nix.enable = true;
+            python.enable = true;
+            html.enable = true;
+            css.enable = true;
+            ts.enable = true;
+            markdown = {
+              enable = true;
+              extensions.markview-nvim.enable = true;
+            };
           };
-        };
-        lsp = {
-          enable = true;
-          formatOnSave = true;
-          inlayHints.enable = true;
-          lightbulb.enable = true;
-          lspSignature.enable = true;
-          lspkind.enable = true;
-          lspsaga.enable = true;
-          mappings = {
-            codeAction = "<leader>ca";
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+            inlayHints.enable = true;
+            lightbulb.enable = true;
+            lspSignature.enable = true;
+            lspkind.enable = true;
+            lspsaga.enable = true;
+            mappings = {
+              codeAction = "<leader>ca";
+            };
           };
         };
       };
     };
+
+    zen-browser = {
+      enable = true;
+      policies = {};
+    };
+
+    home-manager.enable = true;
   };
-
-  programs.zen-browser = {
-    enable = true;
-    policies = {};
-  };
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/mridula/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  programs.home-manager.enable = true;
 }
