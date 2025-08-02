3976eb7cb592b1237d1be05a74cbfca77da794b4
Mrid22 <mridulaga@outlook.com>
2025-08-02 23:33:29 +0200

    added zen browser

--- home.nix
+++ home.nix
@@ -1,22 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  imports = [
+    inputs.zen-browser.homeModules.twilight
+  ];
   home.username = "mridula";
   home.homeDirectory = "/home/mridula";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     spotdl
   ];
@@ -77,6 +69,11 @@
     enable = true;
     flake = "/etc/nixos/";
     clean.enable = true;
+  };
+
+  programs.zen-browser = {
+    enable = true;
+    policies = {};
   };
   
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
