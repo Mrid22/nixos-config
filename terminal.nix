39e74bb97a64be0b6315b6b0c1431e8fbd0de896
Mrid22 <mridulaga@outlook.com>
2025-08-03 00:03:21 +0200

    modularizing config

--- terminal.nix
+++ terminal.nix
@@ -1,3 +1,61 @@
 {config,pkgs,...}{
+  programs.git = {
+    enable = true;
+    userName = "Mrid22";
+    userEmail = "mridulaga@outlook.com";
+    delta.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableZshIntegration = true;
+    enableGitIntegration = true;
+    settings = {
+      background_opacity = 0.5;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git" "sudo"];
+    };
+  };
+
+  programs.oh-my-posh = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.pay-respects.enable = true;
+  programs.atuin.enable = true;
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = [
+      "--cmd cd"
+    ];
+  };
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.cava = {
+    enable = true;
+  };
+
+  programs.nh = {
+    enable = true;
+    flake = "/etc/nixos/";
+    clean.enable = true;
+  };
 
 }
