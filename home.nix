bf8e23a7531603d2139490faa7a054f74f77d97f
Mrid22 <mridulaga@outlook.com>
2025-08-03 10:13:18 +0200

    trying to add quickshell

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
     inputs.zen-browser.homeModules.twilight
     inputs.nvf.homeManagerModules.default
     ./terminal.nix
+    ./de.nix
   ];
 
   home = {
@@ -90,84 +91,5 @@
     };
 
     home-manager.enable = true;
-  };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      general = {
-        gaps_in = 5;
-        gaps_out = 5;
-        border_size = 0;
-        resize_on_border = true;
-      };
-
-      input = {
-        follow_mouse = 1;
-        touchpad.natural_scroll = true;
-      };
-
-      animations = {
-        enabled = "yes, please :)";
-        bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1.0"
-          "quick,0.15,0,0.1,1"
-        ];
-        animation = [
-          "global, 1, 10, default"
-          "border, 1, 5.39, easeOutQuint"
-          "windows, 1, 4.79, easeOutQuint"
-          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-          "windowsOut, 1, 1.49, linear, popin 87%"
-          "fadeIn, 1, 1.73, almostLinear"
-          "fadeOut, 1, 1.46, almostLinear"
-          "fade, 1, 3.03, quick"
-          "layers, 1, 3.81, easeOutQuint"
-          "layersIn, 1, 4, easeOutQuint, fade"
-          "layersOut, 1, 1.5, linear, fade"
-          "fadeLayersIn, 1, 1.79, almostLinear"
-          "fadeLayersOut, 1, 1.39, almostLinear"
-          "workspaces, 1, 1.94, almostLinear, slide"
-          "workspacesIn, 1, 1.21, almostLinear, slide"
-          "workspacesOut, 1, 1.94, almostLinear, slide"
-        ];
-      };
-
-      "$mod" = "ALT";
-
-      bind = [
-        "$mod, Q, exec, kitty"
-        "$mod, F, exec, zen"
-        "$mod, E, exec, dolphin"
-        "$mod, SPACE, exec, gauntlet open"
-        "$mod, C, killactive"
-
-        # Workspaces
-        "$mod, 1, workspace, 1"
-        "$mod, 2, workspace, 2"
-        "$mod, 3, workspace, 3"
-        "$mod, 4, workspace, 4"
-        "$mod, 5, workspace, 5"
-        "$mod, 6, workspace, 3"
-        "$mod, 7, workspace, 7"
-        "$mod, 8, workspace, 8"
-        "$mod, 9, workspace, 9"
-        "$mod, 0, workspace, 0"
-
-        "$mod SHIFT, 1, movetoworkspace, 1"
-        "$mod SHIFT, 2, movetoworkspace, 2"
-        "$mod SHIFT, 3, movetoworkspace, 3"
-        "$mod SHIFT, 4, movetoworkspace, 4"
-        "$mod SHIFT, 5, movetoworkspace, 5"
-        "$mod SHIFT, 6, movetoworkspace, 3"
-        "$mod SHIFT, 7, movetoworkspace, 7"
-        "$mod SHIFT, 8, movetoworkspace, 8"
-        "$mod SHIFT, 9, movetoworkspace, 9"
-        "$mod SHIFT, 0, movetoworkspace, 0"
-      ];
-    };
   };
 }
