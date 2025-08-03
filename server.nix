1d83037f794edecfd5156da6a8d6e24d32d64209
Mrid22 <mridulaga@outlook.com>
2025-08-03 13:39:15 +0200

    typo

--- server.nix
+++ server.nix
@@ -3,12 +3,10 @@
   pkgs,
   ...
 }: {
-  fileSystems."/media/" = {
+  fileSystems."/media" = {
     device = "/dev/sda1";
     fsType = "ntfs";
     options = [
-      # If you don't have this options attribute, it'll default to "defaults"
-      # boot options for fstab. Search up fstab mount options you can use
       "users" # Allows any user to mount and unmount
       "nofail" # Prevent system from failing if this drive doesn't mount
     ];
