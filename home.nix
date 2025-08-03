28e3a939af95ea6a6645f2067ca62c0c724eea07
Mrid22 <mridulaga@outlook.com>
2025-08-03 09:38:42 +0200

    hyprland - added workspace shortcuts

--- home.nix
+++ home.nix
@@ -17,21 +17,6 @@
     packages = with pkgs; [
       spotdl
     ];
-
-    # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
-    file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
-    };
   };
 
   programs = {
@@ -126,6 +111,26 @@
 
         # Workspaces
         "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 3"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 0"
+
+        "$mod, 1, movetoworkspace, 1"
+        "$mod, 2, movetoworkspace, 2"
+        "$mod, 3, movetoworkspace, 3"
+        "$mod, 4, movetoworkspace, 4"
+        "$mod, 5, movetoworkspace, 5"
+        "$mod, 6, movetoworkspace, 3"
+        "$mod, 7, movetoworkspace, 7"
+        "$mod, 8, movetoworkspace, 8"
+        "$mod, 9, movetoworkspace, 9"
+        "$mod, 0, movetoworkspace, 0"
       ];
     };
   };
