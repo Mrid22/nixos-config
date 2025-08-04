a28383dc5b68561187cac64f855148b9ae96bcd4
Mrid22 <mridulaga@outlook.com>
2025-08-04 22:17:17 +0200

    made battery stop charging at 80 for battery health

--- system.nix
+++ system.nix
@@ -52,6 +52,13 @@
     displayManager.gdm.enable = true;
     udisks2.enable = true;
     upower.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+      };
+    };
 
     printing.enable = true;
 
