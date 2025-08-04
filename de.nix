53380457bfc654e2c71362b12d047b81a0ec5ff2
Mrid22 <mridulaga@outlook.com>
2025-08-04 16:22:16 +0200

    typo

--- de.nix
+++ de.nix
@@ -121,7 +121,7 @@
         "$mod, 3, workspace, 3"
         "$mod, 4, workspace, 4"
         "$mod, 5, workspace, 5"
-        "$mod, 6, workspace, 3"
+        "$mod, 6, workspace, 6"
         "$mod, 7, workspace, 7"
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
