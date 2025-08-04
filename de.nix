4e412ca0df271f09325e2311fda88197cd88c80a
Mrid22 <mridulaga@outlook.com>
2025-08-04 22:20:33 +0200

    clean up config according to new nixos options

--- de.nix
+++ de.nix
@@ -8,7 +8,7 @@
     swww.enable = true;
     hyprsunset = {
       enable = true;
-      transitions = {
+      settings = {
         sunrise = {
           calendar = "*-*-* 06:00:00";
           requests = [
