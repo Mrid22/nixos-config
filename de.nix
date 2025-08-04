21383fb0f6e9e41920c70076a5fc95101342f293
Mrid22 <mridulaga@outlook.com>
2025-08-04 22:11:22 +0200

    config hyprsuset

--- de.nix
+++ de.nix
@@ -8,6 +8,21 @@
     swww.enable = true;
     hyprsunset = {
       enable = true;
+      transitions = {
+        sunrise = {
+          calendar = "*-*-* 06:00:00";
+          requests = [
+            ["temperature" "6500"]
+            ["gamma 100"]
+          ];
+        };
+        sunset = {
+          calendar = "*-*-* 19:00:00";
+          requests = [
+            ["temperature" "3500"]
+          ];
+        };
+      };
     };
   };
   programs = {
