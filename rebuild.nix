dbee7abaa1c11e82d2cdbf2343cf9f25c819fda7
Mrid22 <mridulaga@outlook.com>
2025-08-10 17:32:01 +0200

    typo

--- rebuild.nix
+++ rebuild.nix
@@ -3,7 +3,7 @@ pkgs.writeShellScriptBin "rebuild" ''
   ${pkgs.git}/bin/git add --all
   ${pkgs.git}/bin/git diff
   read -p "Commit message: " commitmsg
-  ${pkgs.git}/bin/git commit --all --message "commitmsg"
+  ${pkgs.git}/bin/git commit --all --message "$commitmsg"
   ${pkgs.git}/bin/git push
   ${pkgs.nh}/bin/nh os switch
 ''
