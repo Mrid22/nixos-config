7a46d78d993243b78fcaf48636177d991016e555
Mrid22 <mridulaga@outlook.com>
2025-08-10 21:01:39 +0200

    config open web ui

--- server.nix
+++ server.nix
@@ -36,7 +36,7 @@
             {
               "Ollama" = {
                 description = "Ollama";
-                href = "http://localhost/";
+                href = "http://localhost:8080";
               };
             }
           ];
@@ -83,6 +83,13 @@
     open-webui = {
       enable = true;
       openFirewall = true;
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        WEBUI_AUTH = "False";
+      };
     };
     tailscale = {
       enable = true;
