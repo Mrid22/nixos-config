4824e235033c9ea7be019cbff7a558ac3aa36ba8
Mrid22 <mridulaga@outlook.com>
2025-08-10 16:56:03 +0200

    add script to rebuild system

---
+++ rebuild.nix
@@ -0,0 +1,9 @@
+{pkgs}:
+pkgs.writeShellScript "rebuild" ''
+  ${pkgs.git}/bin/git add --all
+  ${pkgs.git}/bin/git diff
+  read -p "Commit message: " commitmsg
+  ${pkgs.git}/bin/git commit --all --message "commitmsg"
+  ${pkgs.git}/bin/git push
+  ${pkgs.nh}/bin/nh os switch
+''
