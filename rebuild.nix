23bb01f2ff777092da24b642ce4011d81918a184
Mrid22 <mridulaga@outlook.com>
2025-08-10 21:18:41 +0200

    made me confirm diff

--- rebuild.nix
+++ rebuild.nix
@@ -1,9 +1,15 @@
 {pkgs}:
 pkgs.writeShellScriptBin "rebuild" ''
   ${pkgs.git}/bin/git diff
-  ${pkgs.git}/bin/git add --all
-  read -p "Commit message: " commitmsg
-  ${pkgs.git}/bin/git commit --all --message "$commitmsg"
-  ${pkgs.git}/bin/git push
-  ${pkgs.nh}/bin/nh os switch
+  read -p "Diff approved? (y/n): " -n 1 -r CONFIRM
+  if [[ ! $CONFIRM =~ ^[Yy]$ ]]; then
+    echo "Aborting."
+    exit 1
+  else
+    ${pkgs.git}/bin/git add --all
+    read -p "Commit message: " commitmsg
+    ${pkgs.git}/bin/git commit --all --message "$commitmsg"
+    ${pkgs.git}/bin/git push
+    ${pkgs.nh}/bin/nh os switch
+  fi
 ''
