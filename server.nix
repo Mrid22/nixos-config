ffb693ebe52f016a47e4b6f25e3dd9a1fe779bab
Mrid22 <mridulaga@outlook.com>
2025-08-10 21:20:39 +0200

    trying to fix open web ui

--- server.nix
+++ server.nix
@@ -88,7 +88,6 @@
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
         OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-        WEBUI_AUTH = "False";
       };
     };
     tailscale = {
