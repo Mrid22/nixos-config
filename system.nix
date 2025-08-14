49b6ec395923e49460ae08a8632cfa59c8ba1111
Mrid22 <mridulaga@outlook.com>
2025-08-14 11:08:39 +0200

    added flatpak apps declaratively

--- system.nix
+++ system.nix
@@ -139,12 +139,23 @@
       randomizedDelaySec = "45min";
     };
   };
+  systemd = {
+    services = {
+      flatpak-repo = {
+        wantedBy = ["multi-user.target"];
+        path = [pkgs.flatpak];
+        script = ''
+          flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        '';
+      };
 
-  systemd.services.flatpak-repo = {
-    wantedBy = ["multi-user.target"];
-    path = [pkgs.flatpak];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
+      flatpak-apps = {
+        wantedBy = ["multi-user.target"];
+        path = [pkgs.flatpak];
+        script = ''
+          flatpak install --noninteractive flathub us.materialio.Materialious
+        '';
+      };
+    };
   };
 }
