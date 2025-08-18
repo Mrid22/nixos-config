81d45dfed4058d6220003fd6f08298d99eba5cf3
Mrid22 <mridulaga@outlook.com>
2025-08-18 15:19:00 +0200

    remove nushell

--- terminal.nix
+++ terminal.nix
@@ -10,7 +10,7 @@
   ];
   programs = {
     cava.enable = true;
-    # atuin.enable = true;
+    atuin.enable = true;
     pay-respects.enable = true;
     ripgrep.enable = true;
     lazygit.enable = true;
@@ -33,8 +33,6 @@
         background_opacity = 0.5;
       };
     };
-
-    nushell.enable = true;
 
     zsh = {
       enable = true;
