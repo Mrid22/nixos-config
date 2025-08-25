23cc4c1325b376678dc4fbc340c0ef9d048ceace
Mrid22 <mridulaga@outlook.com>
2025-08-25 21:25:19 +0200

    remove update script because autoupdate

--- update.nix
+++
@@ -1,9 +0,0 @@
-{pkgs}:
-pkgs.writeShellScriptBin "update" ''
-    nix flake update
-    ${pkgs.git}/bin/git add --all
-    ${pkgs.git}/bin/git commit --all --message "Update"
-    ${pkgs.git}/bin/git push
-    ${pkgs.nh}/bin/nh os switch
-  fi
-''
