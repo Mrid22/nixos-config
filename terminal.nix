bfbc8414927b6d147195e6f8a620ca247086446a
Mrid22 <mridulaga@outlook.com>
2025-09-05 19:00:20 +0200

    added nh

--- terminal.nix
+++ terminal.nix
@@ -31,4 +31,11 @@
     enable = true;
     enableZshIntegration = true;
   };
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nixos/nixos-config";
+  };
 }
