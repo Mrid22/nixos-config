6b92858933ed67bcb9794839409cf0bcb6a12bfd
Mrid22 <mridulaga@outlook.com>
2025-09-05 20:32:58 +0200

    typo

--- de.nix
+++ de.nix
@@ -32,7 +32,7 @@
         animation = [
           "global, 1, 10, default"
           "border, 1, 5.39, easeOutQuint"
-          "1, 4.79, easeOutQuint"
+          "windows, 1, 4.79, easeOutQuint"
           "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
           "windowsOut, 1, 1.49, linear, popin 87%"
           "fadeIn, 1, 1.73, almostLinear"
