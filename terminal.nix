ef7b9bf716f962df9e396f5ee070e08ab9475eef
Mrid22 <mridulaga@outlook.com>
2025-09-06 08:49:18 +0200

    terminal: added eza

--- terminal.nix
+++ terminal.nix
@@ -3,58 +3,71 @@
   inputs,
   ...
 }: {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    defaultKeymap = "emacs";
-    shellAliases = {
-      "cd" = "z";
-      "cdi" = "zi";
-    };
-    oh-my-zsh = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "sudo"
-      ];
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      defaultKeymap = "emacs";
+      shellAliases = {
+        "cd" = "z";
+        "cdi" = "zi";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "sudo"
+        ];
+      };
     };
-  };
-  programs.kitty = {
-    enable = true;
-    settings = {
-      background_opacity = 0.5;
+
+    kitty = {
+      enable = true;
+      settings = {
+        background_opacity = 0.5;
+      };
     };
-  };
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.oh-my-posh = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.pay-respects = {
-    enable = true;
-    enableZshIntegration = true;
-  };
 
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/etc/nixos/nixos-config";
-  };
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
 
-  programs.git = {
-    enable = true;
-    userName = "Mrid22";
-    userEmail = "mridulaga@outlook.com";
-    delta.enable = true;
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
+    oh-my-posh = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    pay-respects = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "/etc/nixos/nixos-config";
+    };
+
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+      icons = "always";
+      git = true;
+    };
+
+    git = {
+      enable = true;
+      userName = "Mrid22";
+      userEmail = "mridulaga@outlook.com";
+      delta.enable = true;
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
       };
     };
   };
