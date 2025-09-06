20426fad21474e26ef7bdc6f55f9ad1190ca81d9
Mrid22 <mridulaga@outlook.com>
2025-09-06 08:18:49 +0200

    home - nvf: added telescope

--- home.nix
+++ home.nix
@@ -11,68 +11,72 @@
     # inputs.gauntlet.homeManagerModules.default
     inputs.nvf.homeManagerModules.default
   ];
+  home = {
+    # programs.gauntlet = {
+    #   enable = true;
+    #   service.enable = true;
+    #   config = {};
+    # };
+    username = "mridula";
+    homeDirectory = "/home/mridula";
 
-  programs.zen-browser.enable = true;
-  # programs.gauntlet = {
-  #   enable = true;
-  #   service.enable = true;
-  #   config = {};
-  # };
-  home.username = "mridula";
-  home.homeDirectory = "/home/mridula";
+    stateVersion = "25.05";
 
-  home.stateVersion = "25.05";
+    packages = [
+    ];
 
-  home.packages = [
-  ];
-
-  home.sessionVariables = {
+    sessionVariables = {
+    };
   };
+  programs = {
+    zen-browser.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
 
-  programs.nvf = {
-    enable = true;
-    settings = {
-      vim = {
-        viAlias = true;
-        vimAlias = true;
-        withNodeJs = true;
-        statusline.lualine.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        lineNumberMode = "none";
-        options = {
-          tabstop = 2;
-          shiftwidth = 2;
-        };
-        treesitter = {
-          addDefaultGrammars = true;
-          autotagHtml = true;
-          context.enable = true;
-        };
-        theme = {
-          enable = true;
-          name = "tokyonight";
-          style = "storm";
-          transparent = true;
-        };
-        languages = {
-          enableDAP = true;
-          enableExtraDiagnostics = true;
-          enableFormat = true;
-          enableTreesitter = true;
-          # Languages
-          html.enable = true;
-          nix.enable = true;
-          css.enable = true;
-          ts.enable = true;
-          tailwind.enable = true;
-        };
-        lsp = {
-          enable = true;
-          formatOnSave = true;
-          lspkind.enable = true;
+    nvf = {
+      enable = true;
+      settings = {
+        vim = {
+          viAlias = true;
+          vimAlias = true;
+          withNodeJs = true;
+          telescope.enable = true;
+          statusline.lualine.enable = true;
+          autocomplete.nvim-cmp.enable = true;
+          lineNumberMode = "none";
+          options = {
+            tabstop = 2;
+            shiftwidth = 2;
+          };
+          treesitter = {
+            addDefaultGrammars = true;
+            autotagHtml = true;
+            context.enable = true;
+          };
+          theme = {
+            enable = true;
+            name = "tokyonight";
+            style = "storm";
+            transparent = true;
+          };
+          languages = {
+            enableDAP = true;
+            enableExtraDiagnostics = true;
+            enableFormat = true;
+            enableTreesitter = true;
+            # Languages
+            html.enable = true;
+            nix.enable = true;
+            css.enable = true;
+            ts.enable = true;
+            tailwind.enable = true;
+          };
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+            lspkind.enable = true;
+          };
         };
       };
     };
