b8dad7cb8801351981a5e890f8fa7d831360b6c8
Mrid22 <mridulaga@outlook.com>
2025-09-20 19:31:19 +0200

    added servARR apps

--- server.nix
+++ server.nix
@@ -12,8 +12,18 @@
       "nofail"
     ];
   };
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
+  services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+    };
   };
 }
