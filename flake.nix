624eb1871539fd193a3c1b04ea00b5d35032b5e9
Mrid22 <mridulaga@outlook.com>
2025-09-20 12:19:31 +0200

    init nixos config in home directory

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+	    home-manager.extraSpecialArgs = {inherit inputs;};
+
+            home-manager.users.mridula = import ./home.nix;
+
+          }
+        ];
+      };
+    };
+  };
+}
