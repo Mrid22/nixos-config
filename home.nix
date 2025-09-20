64db0bf18c559d334bc07ed8e8eabfefeac32b1d
Mrid22 <mridulaga@outlook.com>
2025-09-20 17:09:06 +0200

    finished moving terminal config

--- home.nix
+++ home.nix
@@ -169,50 +169,6 @@
 
   # basic configuration of git, please change to your own
 
-programs.zsh = {
-  enable = true;
-  autocd = true;
-  defaultKeymap = "emacs";
-  syntaxHighlighting.enable = true;
-  autosuggestion.enable = true;
-  shellAliases = {
-    "cd" = "z";
-    "cdi" = "zi";
-  };
-  oh-my-zsh = {
-    enable = true;
-    plugins = ["git" "sudo"];
-  };
-};
-
-programs.nh = {
-  enable = true;
-  clean.enable = true;
-  flake = "/home/mridula/nixos-config";
-};
-
-programs.kitty = {
-  enable = true;
-  shellIntegration.enableZshIntegration = true;
-  settings = {
-    background_opacity = 0.5;
-  };
-};
-
-programs.oh-my-posh = {
-  enable = true;
-  enableZshIntegration = true;
-};
-
-programs.atuin = {
-  enable = true;
-  enableZshIntegration = true;
-};
-
-programs.zoxide = {
-  enable = true;
-  enableZshIntegration = true;
-};
   # starship - an customizable prompt for any shell
 
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
--- terminal.nix
+++ terminal.nix
@@ -13,4 +13,49 @@ inputs,
       init.defaultBranch = "main";
     };
   };
+
+programs.zsh = {
+  enable = true;
+  autocd = true;
+  defaultKeymap = "emacs";
+  syntaxHighlighting.enable = true;
+  autosuggestion.enable = true;
+  shellAliases = {
+    "cd" = "z";
+    "cdi" = "zi";
+  };
+  oh-my-zsh = {
+    enable = true;
+    plugins = ["git" "sudo"];
+  };
+};
+
+programs.nh = {
+  enable = true;
+  clean.enable = true;
+  flake = "/home/mridula/nixos-config";
+};
+
+programs.kitty = {
+  enable = true;
+  shellIntegration.enableZshIntegration = true;
+  settings = {
+    background_opacity = 0.5;
+  };
+};
+
+programs.oh-my-posh = {
+  enable = true;
+  enableZshIntegration = true;
+};
+
+programs.atuin = {
+  enable = true;
+  enableZshIntegration = true;
+};
+
+programs.zoxide = {
+  enable = true;
+  enableZshIntegration = true;
+};
 }
