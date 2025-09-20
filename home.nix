5f1f27beb009868092999466758f8d859cc5bc76
Mrid22 <mridulaga@outlook.com>
2025-09-20 17:12:50 +0200

    nvf - tokyonight themeing

--- home.nix
+++ home.nix
@@ -104,12 +104,6 @@
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
-
-    neofetch
-
-    # archives
     zip
     xz
     unzip
@@ -122,19 +116,6 @@
     eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
 
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils  # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
-
-    # misc
-    cowsay
-    file
     which
     tree
     gnused
@@ -143,44 +124,18 @@
     zstd
     gnupg
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
-
-    # productivity
-
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-
-    # system tools
-    sysstat
-    lm_sensors # for `sensors` command
-    ethtool
-    pciutils # lspci
-    usbutils # lsusb
   ];
 
-  # basic configuration of git, please change to your own
+programs.nvf = {
+  vim = {
+    theme = {
+      enable = true;
+      transprent = true;
+      name = "tokyonight";
+      style = "night";
+    };
+  };
+};
 
-  # starship - an customizable prompt for any shell
-
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.05";
 }
