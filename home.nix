47c64e170712d5fafdbe278b0b98ccbaafdeca68
Mrid22 <mridulaga@outlook.com>
2025-09-26 21:42:49 +0200

    declareative librewolf

--- home.nix
+++ home.nix
@@ -151,7 +151,42 @@
     librewolf = {
       enable = true;
       profiles.mridula = {
-        extensions = {};
+        search = {
+          default = "ddg";
+          engines = {
+            nix-packages = {
+              name = "Nix Packages";
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+
+            nixos-wiki = {
+              name = "NixOS Wiki";
+              urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
+              iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+              definedAliases = ["@nw"];
+            };
+
+            bing.metaData.hidden = true;
+            google.metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
+        };
       };
     };
     ags = {
