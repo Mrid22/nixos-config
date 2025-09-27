ff019d8de1d6612598b974bf5aff7438a437d5a9
Mrid22 <mridulaga@outlook.com>
2025-09-27 23:26:14 +0200

    moved librewolf config

--- home/librewolf.nix
+++ home/librewolf.nix
@@ -1,1 +1,61 @@
-{}: {}
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.librewolf = {
+    enable = true;
+    settings."extensions.autoDisableScopes" = 0;
+    profiles.mridula = {
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "Colleges";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "Computational Social Science Bsc @ UvA";
+                url = "https://www.uva.nl/en/programmes/bachelors/computational-social-science";
+              }
+              {
+                name = "Computing Science Bsc @ Radboud University";
+                url = "https://www.ru.nl/en/education/bachelors/computing-science";
+              }
+            ];
+          }
+        ];
+      };
+      search = {
+        default = "ddg";
+        engines = {
+          nix-options = {
+            name = "NixOS";
+            urls = [
+              {
+                template = "https://mynixos.com/search";
+                params = [
+                  {
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+
+            definedAliases = ["@nx"];
+          };
+
+          bing.metaData.hidden = true;
+          google.metaData.alias = "@g";
+        };
+      };
+      extensions = {
+        packages = with pkgs.firefoxAddons; [
+          ublock-origin
+          proton-pass
+        ];
+      };
+    };
+  };
+}
