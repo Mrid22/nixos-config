4320271111f37a6941115abae218271f8c5639cc
Mrid22 <mridulaga@outlook.com>
2025-09-27 21:52:16 +0200

    added nvidia support

--- configuration.nix
+++ configuration.nix
@@ -10,31 +10,6 @@
     ./server.nix
     ./system.nix
   ];
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-  networking = {
-    hostName = "nixos"; # Define your hostname.
-    networkmanager.enable = true;
-  };
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  time.timeZone = "Europe/Amsterdam";
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "nl_NL.UTF-8";
-      LC_IDENTIFICATION = "nl_NL.UTF-8";
-      LC_MEASUREMENT = "nl_NL.UTF-8";
-      LC_MONETARY = "nl_NL.UTF-8";
-      LC_NAME = "nl_NL.UTF-8";
-      LC_NUMERIC = "nl_NL.UTF-8";
-      LC_PAPER = "nl_NL.UTF-8";
-      LC_TELEPHONE = "nl_NL.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
   services = {
     tlp.enable = true;
     xserver = {
@@ -105,16 +80,4 @@
     protonvpn-gui
     wl-clipboard
   ];
-  system = {
-    autoUpgrade = {
-      enable = true;
-      flake = inputs.self.outPath;
-      flags = [
-        "-L"
-      ];
-      dates = "02:00";
-      randomizedDelaySec = "45min";
-    };
-    stateVersion = "25.05";
-  };
 }
--- system.nix
+++ system.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: {
   # Bootloader
@@ -10,5 +11,68 @@
       efi.canTouchEfiVariables = true;
     };
     kernelParams = ["snd_hda_intel.power_save=0"];
+  };
+
+  # Hardware Settings
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSuppot32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      prime = {
+        sync.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  # Networking
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true;
+  };
+
+  # Nix Settings
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Locale
+  time.timeZone = "Europe/Amsterdam";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "nl_NL.UTF-8";
+      LC_IDENTIFICATION = "nl_NL.UTF-8";
+      LC_MEASUREMENT = "nl_NL.UTF-8";
+      LC_MONETARY = "nl_NL.UTF-8";
+      LC_NAME = "nl_NL.UTF-8";
+      LC_NUMERIC = "nl_NL.UTF-8";
+      LC_PAPER = "nl_NL.UTF-8";
+      LC_TELEPHONE = "nl_NL.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+  # OS settings
+  system = {
+    autoUpgrade = {
+      enable = true;
+      flake = inputs.self.outPath;
+      flags = [
+        "-L"
+      ];
+      dates = "02:00";
+      randomizedDelaySec = "45min";
+    };
+    stateVersion = "25.05";
   };
 }
