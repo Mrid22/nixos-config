d2593edf0d78d0f39a93b073b95c5ffe0686ebcf
Mrid22 <mridulaga@outlook.com>
2025-09-27 10:04:12 +0200

    server - trying to fix open web ui

--- server.nix
+++ server.nix
@@ -38,6 +38,13 @@
     open-webui = {
       enable = true;
       openFirewall = true;
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+        OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+      };
     };
     ollama = {
       enable = true;
