2f1c1cded1991dd2feeef97425987f4d28666357
Mrid22 <mridulaga@outlook.com>
2025-09-27 22:06:29 +0200

    trying to declare extensions in zen

--- home.nix
+++ home.nix
@@ -42,7 +42,20 @@
   services.kdeconnect.enable = true;
 
   programs = {
-    zen-browser.enable = true;
+    zen-browser = {
+      enable = true;
+      policies = let
+        mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+          installation_mode = "force_installed";
+        });
+      in {
+        ExtensionSettings = mkExtensionSettings {
+          "wappalyzer@crunchlabz.com" = "wappalyzer";
+          "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
+        };
+      };
+    };
     librewolf = {
       enable = true;
       profiles.mridula = {
