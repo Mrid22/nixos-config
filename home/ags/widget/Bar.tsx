92d34ec035d1b811e641809d0de5903122b729ad
Mrid22 <mridulaga@outlook.com>
2025-09-28 13:09:05 +0200

    clean up ags config

--- home/ags/widget/Bar.tsx
+++ home/ags/widget/Bar.tsx
@@ -2,19 +2,11 @@ import app from "ags/gtk4/app"
 import { Astal, Gtk, Gdk } from "ags/gtk4"
 import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
-import Hyprland from "gi://AstalHyprland"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const time = createPoll("", 1000, "date")
   const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-  const hyprland = Hyprland.get_default()
-
-  for (const client of hyprland.get_clients()) {
-    print(client.title)
-  }
-
-  function getActiveWindow() {}
   return (
     <window
       visible
