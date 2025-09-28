ac01c6afa09b0618c3f58f6f2abe7fdf89969586
Mrid22 <mridulaga@outlook.com>
2025-09-28 20:49:58 +0200

    typo

--- home.nix
+++ home.nix
@@ -11,7 +11,7 @@
     ./home/zen.nix
     inputs.nvf.homeManagerModules.default
     inputs.ags.homeManagerModules.default
-    inputs.gauntlet-path.homeManagerModules.default
+    inputs.gauntlet-patch.homeManagerModules.default
   ];
   home = {
     username = "mridula";
