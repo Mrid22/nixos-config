36aa3391e6031c3d11051e63353309c7ee458d77
Mrid22 <mridulaga@outlook.com>
2025-10-01 19:55:23 +0200

    i gotta stop distro switching

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,14 +24,16 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  fileSystems."/var/lib/docker/overlay2/edc6c84dc0f285801a3cf3e007bc7a740b7c2edddc7ac5eb2bcf2208dba34423/merged" =
-    { device = "overlay";
-      fsType = "overlay";
-    };
-
   swapDevices =
     [ { device = "/dev/disk/by-uuid/86096019-7502-4dbf-9113-87fedebd589c"; }
     ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
