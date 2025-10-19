a2e232e62c97b8878861c57517652b31176fd96c
Mrid22 <mridulaga@outlook.com>
2025-10-19 20:21:19 +0200

    added homelab/server

--- server.nix
+++ server.nix
@@ -5,7 +5,33 @@
    options = [
      "users"
      "nofail"
-     
    ];
+ };
+ system.activationScripts.setupFolders =
+    # bash
+    ''
+      mkdir -p /media/downloads/downloads
+      mkdir -p /media/downloads/incomplete
+      mkdir -p /media/tv
+      mkdir -p /media/photos
+    '';
+}
+ services = {
+   sonarr = {
+     enable = true;
+     openFirewall = true;
+   };
+   transmission = {
+     enable = true;
+     package = pkgs.transmission_4;
+     settings = {
+       download-dir = "/media/downloads/downloads";
+       incomplete-dir = "/media/downloads/incomplete";
+       incomplete-dir-enabled = true;
+     };
+   };
+   declarative-jellyfin = {
+     enable = true;
+   };
  };
 }
