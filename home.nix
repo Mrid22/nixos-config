b46f3be25a0112abe950aae02a43bf49212a217d
Mrid22 <mridulaga@outlook.com>
2025-10-20 21:28:04 +0200

    added volume keybinds

--- home.nix
+++ home.nix
@@ -64,6 +64,11 @@
         "$mod SHIFT, 9, movetoworkspace, 9"
         "$mod SHIFT, 0, movetoworkspace, 10"
       ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
     };
   };
   programs = {
