19725055aec93dbc3bd8e243c00a2a0bda61a1e4
Mrid22 <mridulaga@outlook.com>
2025-10-21 19:38:50 +0200

    trying to make home assistant function

--- homelab.nix
+++ homelab.nix
@@ -32,19 +32,22 @@
     };
     home-assistant = {
       enable = true;
-      configWritable = true;
-      openFirewall = true;
-      config = {
-        homeassistant.name = "Home";
-      };
       extraComponents = [
         "analytics"
-        "default_config"
-        "esphome"
-        "my"
+        "google_translate"
+        "met"
+        "radio_browser"
         "shopping_list"
-        "wled"
+        # Recommended for fast zlib compression
+        # https://www.home-assistant.io/integrations/isal
+        "isal"
+        "tuya"
       ];
+      config = {
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = { };
+      };
     };
     prowlarr = {
       enable = true;
