f24e19aeaf94129a12bc34ab010ad9ebc1c48ea3
Mrid22 <mridulaga@outlook.com>
2025-10-21 19:13:13 +0200

    typo

--- homelab.nix
+++ homelab.nix
@@ -38,32 +38,32 @@
       config = {
         homeassistant.name = "Home";
       };
-      # defaultIntegrations = [
-      #   "application_credentials"
-      #   "androidtv_remote"
-      #   "frontend"
-      #   "hardware"
-      #   "logger"
-      #   "network"
-      #   "system_health"
-      #   "automation"
-      #   "person"
-      #   "scene"
-      #   "script"
-      #   "tag"
-      #   "zone"
-      #   "counter"
-      #   "input_boolean"
-      #   "input_button"
-      #   "input_datetime"
-      #   "input_number"
-      #   "input_select"
-      #   "input_text"
-      #   "schedule"
-      #   "timer"
-      #   "tuya"
-      #   "backup"
-      # ];
+      defaultIntegrations = [
+        "application_credentials"
+        "androidtv_remote"
+        "frontend"
+        "hardware"
+        "logger"
+        "network"
+        "system_health"
+        "automation"
+        "person"
+        "scene"
+        "script"
+        "tag"
+        "zone"
+        "counter"
+        "input_boolean"
+        "input_button"
+        "input_datetime"
+        "input_number"
+        "input_select"
+        "input_text"
+        "schedule"
+        "timer"
+        "tuya"
+        "backup"
+      ];
     };
     prowlarr = {
       enable = true;
