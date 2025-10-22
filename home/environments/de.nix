159dc8f0499df7b9bcb0add6cd6d5cd659754d90
Mrid22 <mridulaga@outlook.com>
2025-10-22 14:30:31 +0200

    config hypridle

--- home/environments/de.nix
+++ home/environments/de.nix
@@ -52,6 +52,17 @@
           ignore_dbus_inhibit = false;
           lock_cmd = "hyprlock";
         };
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
       };
     };
   };
