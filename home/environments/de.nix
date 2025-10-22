0b9a5117399299dc64f64078e8d81c7094889162
Mrid22 <mridulaga@outlook.com>
2025-10-22 20:22:41 +0200

    same

--- home/environments/de.nix
+++ home/environments/de.nix
@@ -83,18 +83,6 @@
             bezier = "easeOutQuint";
           };
         };
-        input-field = [
-          {
-            size = "200, 50";
-            position = "0, -80";
-            monitor = "";
-            dots_center = true;
-            fade_on_empty = false;
-            outline_thickness = 5;
-            placeholder_text = "Password...";
-            shadow_passes = 2;
-          }
-        ];
       };
     };
     hyprpanel = {
