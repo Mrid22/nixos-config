7fc5385bcbecbe4dbc26013067504597e6756bee
Mrid22 <mridulaga@outlook.com>
2025-10-22 12:08:44 +0200

    added hyprsunset for bluelight filtering

--- home/environments/de.nix
+++ home/environments/de.nix
@@ -26,6 +26,24 @@
         wallpaper = [ ",~/nixos-config/Wallpaper.jpg" ];
       };
     };
+    hyprsunset = {
+      enable = true;
+      settings = {
+        max-gamma = 150;
+
+        profile = [
+          {
+            time = "7:30";
+            identity = true;
+          }
+          {
+            time = "21:00";
+            temperature = 5000;
+            gamma = 0.8;
+          }
+        ];
+      };
+    };
   };
   programs = {
     hyprshot = {
