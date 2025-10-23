8c84576f8a75c2dbac54c9ea3ea8d83bb5b1e7cc
Mrid22 <mridulaga@outlook.com>
2025-10-23 21:56:51 +0200

    switched to home assistant oci container

--- homelab.nix
+++ homelab.nix
@@ -20,6 +20,21 @@
     mkdir -p /media/photos
     mkdir -p /media/home
   '';
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = ["/media/home:/config"];
+      environment.TZ = "Europe/Amsterdam";
+      # Note: The image will not be updated on rebuilds, unless the version label changes
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      extraOptions = [
+        # Use the host network namespace for all sockets
+        "--network=host"
+        # Pass devices into the container, so Home Assistant can discover and make use of them
+        "--device=/dev/ttyACM0:/dev/ttyACM0"
+      ];
+    };
+  };
   services = {
     sonarr = {
       enable = true;
@@ -78,19 +93,19 @@
       openFirewall = true;
       host = "0.0.0.0";
     };
-    home-assistant = {
-      enable = true;
-      extraComponents = [
-        "analytics"
-        "google_translate"
-        "met"
-        "radio_browser"
-        "shopping_list"
-        "isal"
-      ];
-      config = {
-        default_config = {};
-      };
-    };
+    # home-assistant = {
+    #   enable = true;
+    #   extraComponents = [
+    #     "analytics"
+    #     "google_translate"
+    #     "met"
+    #     "radio_browser"
+    #     "shopping_list"
+    #     "isal"
+    #   ];
+    #   config = {
+    #     default_config = {};
+    #   };
+    # };
   };
 }
