5dacecf81adcda6cbc379ec3f2b87a30b0aabc50
Mrid22 <mridulaga@outlook.com>
2025-10-23 12:49:10 +0200

    remove nix colors

--- flake.nix
+++ flake.nix
@@ -28,11 +28,6 @@
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
