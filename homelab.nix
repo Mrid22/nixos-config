0036cd6727a44ab1e4d30752cb2b6408522e8b21
Mrid22 <mridulaga@outlook.com>
2025-10-23 22:03:31 +0200

    forgot to bind home assistant to a port

--- homelab.nix
+++ homelab.nix
@@ -24,13 +24,18 @@
     backend = "docker";
     containers.homeassistant = {
       volumes = ["/media/home:/config"];
-      environment.TZ = "Europe/Amsterdam";
+      environment = {
+        TZ = "Europe/Amsterdam";
+        PUID = toString config.users.mridula.uid;
+        GUID = toString config.users.mridula.gid;
+      };
       image = "ghcr.io/home-assistant/home-assistant:stable";
+      ports = [
+        "8123:8123"
+      ];
       extraOptions = [
-        # Use the host network namespace for all sockets
         "--network=host"
-        # Pass devices into the container, so Home Assistant can discover and make use of them
-        "--device=/dev/ttyACM0:/dev/ttyACM0"
+        "--pull=newer"
       ];
     };
   };
