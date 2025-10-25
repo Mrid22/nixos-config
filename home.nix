f0d0ec9eba86ab3d4ddf1e2aa22cc4deed13c43b
Mrid22 <mridulaga@outlook.com>
2025-10-25 19:19:34 +0200

    trying to go back to zen browser because the extension toolbar button might have been fixed after the new update that moved it elsewhere

--- home.nix
+++ home.nix
@@ -9,6 +9,7 @@
     ./home/environments/terminal.nix
     ./home/apps/floorp.nix
     ./home/apps/nvf.nix
+    inputs.zen-browser.homeModules.twilight
   ];
   home = {
     username = "mridula";
@@ -28,6 +29,57 @@
     };
   };
   programs = {
+    zen-browser = {
+      enable = true;
+      profiles.mridula = {
+        settings."extensions.autoDisableScopes" = 0;
+        extensions = {
+          force = true;
+          packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+            ublock-origin
+            privacy-badger
+            proton-pass
+            ctrl-number-to-switch-tabs
+            dearrow
+            decentraleyes
+            sponsorblock
+            duckduckgo-privacy-essentials
+            #improved-tube
+          ];
+        };
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "Dropout";
+              url = "watch.dropout.tv";
+            }
+          ];
+        };
+        search = {
+          default = "ddg";
+          engines = {
+            MyNixOS = {
+              name = "MyNixOS";
+              definedAliases = ["!nx"];
+              urls = [
+                {
+                  template = "https://mynixos.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            };
+          };
+        };
+      };
+    };
     home-manager.enable = true;
   };
 }
