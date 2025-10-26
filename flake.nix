96329f05ffa98cc9dc1bb88c5a3d8a72a74a2129
Mrid22 <mridulaga@outlook.com>
2025-10-26 11:18:14 +0100

    forgot to readd shb flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    selfhostblocks.url = "github:ibizaman/selfhostblocks";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
