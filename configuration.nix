7758d917cb4a274aa998c7609aa199fdd30b0b47
Mrid22 <mridulaga@outlook.com>
2025-10-28 18:56:26 +0100

    have to do flake first

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     ./config/homelab.nix
     ./config/system.nix
     inputs.sops-nix.nixosModules.sops
-    inputs.selfhostblocks.nextcloud
+    #   inputs.selfhostblocks.nextcloud
   ];
   services.keyd = {
     enable = true;
