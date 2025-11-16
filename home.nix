ab5f178273b3284c44996ceb78ad51b4246e72ff
Mrid22 <mridulaga@outlook.com>
2025-11-16 12:29:04 +0100

    had to reset and force because of a bootloader version mismatch preventing git cloning

---
+++ home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "mridula";
+  home.homeDirectory = "/home/mridula";
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.packages = [
+  ];
+  programs.git = {
+   enable = true;
+   settings = {
+     init.defaultBranch = "main";
+     user = {
+       email = "mridulaga@outlook.com";
+       name = "Mrid22";
+     };
+   };
+  };
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+}
