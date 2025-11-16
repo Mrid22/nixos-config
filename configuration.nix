f79350a2a303833bf40ab120b9f46abe573819d6
Mrid22 <mridulaga@outlook.com>
2025-11-16 15:14:30 +0100

    typo

--- configuration.nix
+++ configuration.nix
@@ -19,17 +19,68 @@
     "nix-command"
     "flakes"
   ];
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    # Bootloader.
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  services.jellyfin.enable = true;
+  networking.hostName = "nixos";
+  services = {
+    # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    jellyfin.enable = true;
+
+    keyd = {
+      enable = true;
+      keyboards = {
+        # The name is just the name of the configuration file, it does not really matter
+        default = {
+          ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
+          # Everything but the ID section:
+          settings = {
+            # The main layer, if you choose to declare it in Nix
+            main = {
+              capslock = "overload(control,escape)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
+            };
+          };
+        };
+      };
+    };
+
+    # Enable the X11 windowing system.
+    xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    # Configure keymap in X11
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable sound with pipewire.
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+  };
+
   gaming = {
     enable = true;
     nvidia.enable = true;
   };
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
@@ -37,27 +88,9 @@
   # Enable networking
 
   home-manager = {
-    # also pass inputs to home-manager modules
     extraSpecialArgs = { inherit inputs; };
     users = {
       "mridula" = import ./home.nix;
-    };
-  };
-
-  services.keyd = {
-    enable = true;
-    keyboards = {
-      # The name is just the name of the configuration file, it does not really matter
-      default = {
-        ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
-        # Everything but the ID section:
-        settings = {
-          # The main layer, if you choose to declare it in Nix
-          main = {
-            capslock = "overload(control,escape)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
-          };
-        };
-      };
     };
   };
 
@@ -80,43 +113,8 @@
     LC_TELEPHONE = "nl_NL.UTF-8";
     LC_TIME = "nl_NL.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mridula = {
     isNormalUser = true;
     description = "Mridul Agarwal";
@@ -124,53 +122,23 @@
       "networkmanager"
       "wheel"
     ];
-    packages = with pkgs; [
-      #  thunderbird
-    ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
-  # Allow unfree packages
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+    firefox.enable = true;
+  };
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
     playerctl
     wl-clipboard
     kitty
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 
 }
--- gaming.nix
+++ gaming.nix
@@ -5,52 +5,54 @@
   lib,
   ...
 }:
-{
-  options.gaming = {
-    enable = lib.mkEnableOption "enable general gaming stuff" lib.mkDefault true;
-    nvidia.enable =
-      lib.mkEnableOption "enable nvidia incase im moving to a computer without it" lib.mkDefault
-        false;
-  };
-
-  config = {
-    gaming = lib.mkIf config.gaming.enable {
-      hardware = {
-        opengl = {
-          enable = true;
-          driSupport = true;
-          driSupport32Bit = true;
-        };
-
-      };
-      programs = {
-        steam = {
-          enable = true;
-          gameScopeSession.enable = true;
-        };
-        gamemode.enable = true;
+let
+  gaming = lib.mkIf config.gaming.enable {
+    hardware = {
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
       };
 
-      environment.systemPackages = with pkgs; [
-        mangohud
-      ];
     };
-
-    nvidia = lib.mkIf config.gaming.nvidia.enable {
-      services.xserver.videoDrivers = [ "nvidia" ];
-      nvidia = {
-        modesetting.enable = true;
-        open = true;
-        powerManagement.enable = true;
-        powerManagement.finegrained = true;
-
-        prime.offload = {
+    programs = {
+      steam = {
+        enable = true;
+        gameScopeSession.enable = true;
+      };
+      gamemode.enable = true;
+    };
+    environment.systemPackages = with pkgs; [
+      mangohud
+    ];
+  };
+  nvidia = lib.mkIf config.gaming.nvidia {
+    services.xserver.videoDrivers = [ "nvidia" ];
+    nvidia = {
+      modesetting.enable = true;
+      open = true;
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+      prime = {
+        offload = {
           enable = true;
           enableOffloadCmd = true;
         };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
       };
-      nvidia.prime.intelBusId = "PCI:0:2:0";
-      nvidia.prime.nvidiaBusId = "PCI:1:0:0";
     };
   };
+in
+{
+  options.gaming = {
+    enable = lib.mkEnableOption "enable module ";
+    nvidia = lib.mkEnableOption "enable nvidia incase im moving to a computer without it";
+  };
+  config = lib.mkMerge [
+    (lib.mkIf config.gaming.enable gaming)
+    (lib.mkIf (config.gaming.enable && config.gaming.nvidia) nvidia)
+  ];
 }
