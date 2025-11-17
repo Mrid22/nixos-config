aadb46552145e9e62a9f319f52e0808aabc1ed0e
Mrid22 <mridulaga@outlook.com>
2025-11-17 22:00:33 +0100

    trying to add shb

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    selfhostblocks.url = "github:ibizaman/selfhostblocks";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,14 +14,17 @@
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     vicinae = {
       url = "github:vicinaehq/vicinae";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -31,19 +35,25 @@
     self,
     nixpkgs,
     nvf,
+    selfhostblocks,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    system = "x86_64-linux";
+    shb = selfhostblocks.lib.${system};
+  in {
     packages."x86_64-linux".default =
       (nvf.lib.neovimConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [./home/apps/nvf.nix];
       }).neovim;
 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = shb.pkgs.nixosSystem {
       specialArgs = {inherit inputs;};
+      inherit system;
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
+        selfhostblocks.nixosModules.default
       ];
     };
   };
