df1d9d19c824619145c4976948c611a5897f3889
Mrid22 <mridulaga@outlook.com>
2025-11-18 11:22:11 +0100

    rebuild: added git diff

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -1,8 +1,11 @@
 {pkgs}:
 pkgs.writeShellScriptBin "rebuild" ''
+  cd ~/nixos-config/
+  ${pkgs.git}/bin/git diff
   ${pkgs.git}/bin/git add .
   main=$(${pkgs.gum}/bin/gum choose $(ls -d */) "typo")
   echo $main
-  second=$(${pkgs.gum}/bin/gum choose $(ls -d ./$"main"*/))
+  ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m $main -m ">" -m "$second"
+  second=$(${pkgs.gum}/bin/gum choose $(ls -d ./"$main"*/))
   ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m $main -m ">" -m "$second"
 ''
