f982f3828feea9eb361fe4a0d4d2c59c4c0d6758
Mrid22 <mridulaga@outlook.com>
2025-11-18 11:28:31 +0100

    typo

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -5,7 +5,6 @@ pkgs.writeShellScriptBin "rebuild" ''
   ${pkgs.git}/bin/git add .
   main=$(${pkgs.gum}/bin/gum choose $(ls -d */) "typo")
   echo $main
-  ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m $main -m ">" -m "$second"
   second=$(${pkgs.gum}/bin/gum choose $(ls -d ./"$main"*))
-  ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m $main -m ">" -m "$second"
+  ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m ""$main"/"second": ${pkgs.gum}/bin/gum input"
 ''
