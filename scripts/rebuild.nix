09dd9980187f8f43849442deeeaf11d10d5b2315
Mrid22 <mridulaga@outlook.com>
2025-11-18 11:20:00 +0100

    test

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -3,10 +3,6 @@ pkgs.writeShellScriptBin "rebuild" ''
   ${pkgs.git}/bin/git add .
   main=$(${pkgs.gum}/bin/gum choose $(ls -d */) "typo")
   echo $main
-  if [[ "$main" == "config" ]]; then
-    second=$(${pkgs.gum}/bin/gum choose $(ls -d ./config/*/))
-  elif [[ "$main" == "home" ]]; then
-    second=$(${pkgs.gum}/bin/gum choose $(ls -d ./home/*/*))
-  fi
+  second=$(${pkgs.gum}/bin/gum choose $(ls -d ./$"main"*/))
   ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m $main -m ">" -m "$second"
 ''
