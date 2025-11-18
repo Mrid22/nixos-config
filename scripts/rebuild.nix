8745176a8e7edc143f778d97c205ac924b70f87c
Mrid22 <mridulaga@outlook.com>
2025-11-18 11:05:34 +0100

    typo

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -2,10 +2,10 @@
 pkgs.writeShellScriptBin "rebuild" ''
   main=$(${pkgs.gum}/bin/gum choose "home" "config")
   echo $main
-  if $main == config:
+  if [[ "$main" == "config" ]]:
   then
     second=$(${pkgs.gum}/bin/gum choose "gaming" "system" "apps")
-  elif $main == home:
+  elif [[ "$main" == "home" ]]:
   then
     second=$(${pkgs.gum}/bin/gum choose "de" "terminal" "zen" "nvf")
   fi
