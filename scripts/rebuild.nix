1f53156d0b2e46d498d20e2a9ae8684258f93ca8
Mrid22 <mridulaga@outlook.com>
2025-11-18 11:06:51 +0100

    typo

--- scripts/rebuild.nix
+++ scripts/rebuild.nix
@@ -2,11 +2,9 @@
 pkgs.writeShellScriptBin "rebuild" ''
   main=$(${pkgs.gum}/bin/gum choose "home" "config")
   echo $main
-  if [[ "$main" == "config" ]]:
-  then
+  if [[ "$main" == "config" ]]; then
     second=$(${pkgs.gum}/bin/gum choose "gaming" "system" "apps")
-  elif [[ "$main" == "home" ]]:
-  then
+  elif [[ "$main" == "home" ]]; then
     second=$(${pkgs.gum}/bin/gum choose "de" "terminal" "zen" "nvf")
   fi
   ${pkgs.gum}/bin/gum confirm "Commit changes?" && git commit -m $main -m ">" - "$second"
