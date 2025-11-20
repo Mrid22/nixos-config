b8ac30870dc1fd2e90301868e3aa7bd46f97b01f
Mrid22 <mridulaga@outlook.com>
2025-11-20 20:03:04 +0100

    config//./config/homelab: Added sample cloudflared config

--- config/homelab/homelab.nix
+++ config/homelab/homelab.nix
@@ -9,6 +9,16 @@
     inputs.sops-nix.nixosModules.default
   ];
 
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "00000000-0000-0000-0000-000000000000" = {
+        credentialsFile = "${config.sops.secrets.cloudflared-creds.path}";
+        default = "http_status:404";
+      };
+    };
+  };
+
   sops = {
     defaultSopsFile = ./secrets.yaml;
     defaultSopsFormat = "yaml";
