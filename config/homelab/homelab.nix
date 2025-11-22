01e1b68a803951127937f5d4fc053cfb68bbe50e
Mrid22 <mridulaga@outlook.com>
2025-11-22 12:13:22 +0100

    config sops

--- config/homelab/homelab.nix
+++ config/homelab/homelab.nix
@@ -4,4 +4,13 @@
   inputs,
   ...
 }: {
+  imports = with inputs; [
+    selfhostblocks.nixosModules.default
+    sops-nix.nixosModules.default
+  ];
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "/home/mridula/.config/sops/age/keys.txt";
+  };
 }
