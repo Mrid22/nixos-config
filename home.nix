f4d2167bf860c431aa1e3cfb28388fd18d7f7b0c
Mrid22 <mridulaga@outlook.com>
2025-11-23 19:43:28 +0100

    typo

--- home.nix
+++ home.nix
@@ -94,7 +94,7 @@
     waybar = {
       enable = true;
       systemd.enable = true;
-      settings = {
+      settings.mainBar = {
         position = "top";
         modules-left = [
           "hyprland/workspaces"
@@ -109,7 +109,7 @@
           "clock"
         ];
         clock = {
-          format = "<span foreground=>   </span>{:%a %d %H:%M}";
+          format = "<span foreground='#f5c2e7'>   </span>{:%a %d %H:%M}";
           tooltip-format = "<big>{:%Y %B}</big>
 <tt><small>{calendar}</small></tt>";
         };
@@ -118,13 +118,13 @@
             warning = 30;
             critical = 15;
           };
-          format = "<span size='13000' foreground=>{icon} </span> {capacity}%";
-          format-warning = "<span size='13000' foreground=>{icon} </span> {capacity}%";
-          format-critical = "<span size='13000' foreground=>{icon} </span> {capacity}%";
-          format-charging = "<span size='13000' foreground=> </span>{capacity}%";
-          format-plugged = "<span size='13000' foreground=> </span>{capacity}%";
-          format-alt = "<span size='13000' foreground=>{icon} </span> {time}";
-          format-full = "<span size='13000' foreground=> </span>{capacity}%";
+          format = "<span size='13000' foreground='#a6e3a1'>{icon} </span> {capacity}%";
+          format-warning = "<span size='13000' foreground='#B1E3AD'>{icon} </span> {capacity}%";
+          format-critical = "<span size='13000' foreground='#E38C8F'>{icon} </span> {capacity}%";
+          format-charging = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
+          format-plugged = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
+          format-alt = "<span size='13000' foreground='#B1E3AD'>{icon} </span> {time}";
+          format-full = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
           format-icons = [
             ""
             ""
@@ -135,10 +135,10 @@
           tooltip-format = "{time}";
         };
         network = {
-          format-wifi = "<span size='13000' foreground=>  </span>{essid}";
-          format-ethernet = "<span size='13000' foreground=>󰤭  </span> Disconnected";
+          format-wifi = "<span size='13000' foreground='#f5e0dc'>  </span>{essid}";
+          format-ethernet = "<span size='13000' foreground='#f5e0dc'>󰤭  </span> Disconnected";
           format-linked = "{ifname} (No IP) ";
-          format-disconnected = "<span size='13000' foreground=>  </span>Disconnected";
+          format-disconnected = "<span size='13000' foreground='#f5e0dc'>  </span>Disconnected";
           tooltip-format-wifi = "Signal Strenght: {signalStrength}%";
         };
         pulseaudio = {
