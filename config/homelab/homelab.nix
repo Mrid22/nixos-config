4610d9a4ceb92e2f8404409325a520199e060339
Mrid22 <mridulaga@outlook.com>
2025-11-23 14:03:01 +0100

    trying to fix cloudflared

--- config/homelab/homelab.nix
+++ config/homelab/homelab.nix
@@ -12,7 +12,7 @@
     defaultSopsFile = ./secrets.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/home/mridula/.config/sops/age/keys.txt";
-    secrets.cloudflare-creds = {};
+    secrets."cloudflared/cert".path = "${config.home.homeDirectory}/.cloudflared/cert.pem";
   };
 
   services = {
@@ -20,7 +20,7 @@
       enable = true;
       tunnels = {
         "d730b790-900f-4664-a8de-fdbaac137fee" = {
-          credentialsFile = "/home/mridula/.cloudflared/cert.pem";
+          credentialsFile = config.sops.secrets."cloudflared/cert".path;
           default = "http_status:404";
         };
       };
