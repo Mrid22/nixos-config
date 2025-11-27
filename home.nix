187d620a5abb5d00eb0af9a8c61eef7f9c033dd5
Mrid22 <mridulaga@outlook.com>
2025-11-27 19:09:00 +0100

    typo

--- home.nix
+++ home.nix
@@ -65,7 +65,7 @@
       cli = {
         enable = true; # Also add caelestia-cli to path
         settings = {
-          theme.enableGtk = true;
+          theme.enableGtk = false;
         };
       };
     };
