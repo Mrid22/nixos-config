db2696f96f6fc1b8a9d416803fd18baa18af79d6
Mrid22 <mridulaga@outlook.com>
2025-11-27 17:57:12 +0100

    waybar: remove color

--- home.nix
+++ home.nix
@@ -111,7 +111,7 @@
           "clock"
         ];
         clock = {
-          format = "<span foreground='#f5c2e7'>   </span>{:%a %d %H:%M}";
+          format = "<span>   </span>{:%a %d %H:%M}";
           tooltip-format = "<big>{:%Y %B}</big>
 <tt><small>{calendar}</small></tt>";
         };
@@ -120,13 +120,13 @@
             warning = 30;
             critical = 15;
           };
-          format = "<span size='13000' foreground='#a6e3a1'>{icon} </span> {capacity}%";
-          format-warning = "<span size='13000' foreground='#B1E3AD'>{icon} </span> {capacity}%";
-          format-critical = "<span size='13000' foreground='#E38C8F'>{icon} </span> {capacity}%";
-          format-charging = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
-          format-plugged = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
-          format-alt = "<span size='13000' foreground='#B1E3AD'>{icon} </span> {time}";
-          format-full = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
+          format = "<span size='13000' >{icon} </span> {capacity}%";
+          format-warning = "<span size='13000' >{icon} </span> {capacity}%";
+          format-critical = "<span size='13000' >{icon} </span> {capacity}%";
+          format-charging = "<span size='13000' > </span>{capacity}%";
+          format-plugged = "<span size='13000' > </span>{capacity}%";
+          format-alt = "<span size='13000' >{icon} </span> {time}";
+          format-full = "<span size='13000' > </span>{capacity}%";
           format-icons = [
             ""
             ""
@@ -137,10 +137,10 @@
           tooltip-format = "{time}";
         };
         network = {
-          format-wifi = "<span size='13000' foreground='#f5e0dc'>  </span>{essid}";
-          format-ethernet = "<span size='13000' foreground='#f5e0dc'>󰤭  </span> Disconnected";
+          format-wifi = "<span size='13000' >  </span>{essid}";
+          format-ethernet = "<span size='13000' >󰤭  </span> Disconnected";
           format-linked = "{ifname} (No IP) ";
-          format-disconnected = "<span size='13000' foreground='#f5e0dc'>  </span>Disconnected";
+          format-disconnected = "<span size='13000' >  </span>Disconnected";
           tooltip-format-wifi = "Signal Strenght: {signalStrength}%";
         };
         pulseaudio = {
