cb23dc37aa418a7ec7ff70f16dba1ff0aaadd388
Mrid22 <mridulaga@outlook.com>
2025-11-29 17:29:03 +0100

    vicinae: migrate extensions to new format

--- home/environments/de.nix
+++ home/environments/de.nix
@@ -17,40 +17,10 @@
         popToRootOnClose = false;
         rootSearch.searchFiles = true;
       };
-      extensions = [
-        (inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system}.mkVicinaeExtension {
-          pname = "bluetooth";
-          src =
-            pkgs.fetchFromGitHub {
-              owner = "vicinaehq";
-              repo = "extensions";
-              rev = "ec7334e9bb636f4771580238bd3569b58dbce879";
-              sha256 = "sha256-C2b6upygLE6xUP/cTSKZfVjMXOXOOqpP5Xmgb9r2dhA=";
-            }
-            + "/extensions/bluetooth";
-        })
-        (inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system}.mkVicinaeExtension {
-          pname = "power-profiles";
-          src =
-            pkgs.fetchFromGitHub {
-              owner = "vicinaehq";
-              repo = "extensions";
-              rev = "effaaa2a87227a3887a263b158b29dd06840932a";
-              sha256 = "sha256-C2b6upygLE6xUP/cTSKZfVjMXOXOOqpP5Xmgb9r2dhA=";
-            }
-            + "/extensions/power-profile";
-        })
-        (inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system}.mkVicinaeExtension {
-          pname = "nix";
-          src =
-            pkgs.fetchFromGitHub {
-              owner = "vicinaehq";
-              repo = "extensions";
-              rev = "610459553a20cf510fa414844f0d094f14ae9643";
-              sha256 = "sha256-C2b6upygLE6xUP/cTSKZfVjMXOXOOqpP5Xmgb9r2dhA=";
-            }
-            + "/extensions/nix";
-        })
+      extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
+        bluetooth
+        nix
+        power-profile
       ];
     };
     hyprpaper = {
