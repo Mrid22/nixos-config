90e10299fbf66f3a9e02208e11be9f2ae950bf11
Mrid22 <mridulaga@outlook.com>
2025-11-29 11:38:11 +0100

    nvf - added dashboard and explorer

--- home/apps/nvf.nix
+++ home/apps/nvf.nix
@@ -1,37 +1,39 @@
 {
-config,
+  config,
   pkgs,
   inputs,
   ...
 }: {
-  imports = [ inputs.nvf.homeManagerModules.default ];
-programs.nvf = {
-enable = true;
-settings = {
-  vim = {
-    viAlias = true;
-    vimAlias = true;
-    lineNumberMode = "none";
-    options = {
-      tabstop = 2;
-      shiftwidth = 2;
+  imports = [inputs.nvf.homeManagerModules.default];
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim = {
+        viAlias = true;
+        vimAlias = true;
+        lineNumberMode = "none";
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
+        statusline.lualine.enable = true;
+        filetree.neo-tree.enable = true;
+        dashboard.alpha.enable = true;
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+        };
+        languages = {
+          enableFormat = true;
+          enableTreesitter = true;
+          nix.enable = true;
+        };
+        autopairs.nvim-autopairs.enable = true;
+        autocomplete.blink-cmp = {
+          enable = true;
+          friendly-snippets.enable = true;
+        };
+      };
     };
-    statusline.lualine.enable = true;
-    lsp = {
-      enable = true;
-      formatOnSave = true;
-    };
-    languages = {
-      enableFormat = true;
-      enableTreesitter = true;
-      nix.enable = true;
-    };
-    autopairs.nvim-autopairs.enable = true;
-    autocomplete.blink-cmp = {
-      enable = true;
-      friendly-snippets.enable = true;
-    };
-  };
-  };
   };
 }
