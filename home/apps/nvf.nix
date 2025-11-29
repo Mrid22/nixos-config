80526fd1ad1a107d995170d5df7774e7a565e712
Mrid22 <mridulaga@outlook.com>
2025-11-29 11:40:38 +0100

    switch

--- home/apps/nvf.nix
+++ home/apps/nvf.nix
@@ -17,7 +17,7 @@
           shiftwidth = 2;
         };
         statusline.lualine.enable = true;
-        filetree.neo-tree.enable = true;
+        filetree.nvim-tree.enable = true;
         dashboard.alpha.enable = true;
         lsp = {
           enable = true;
