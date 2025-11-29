3f590d7114837419319db42e4dc536a07bb51fa2
Mrid22 <mridulaga@outlook.com>
2025-11-29 14:21:49 +0100

    trying to fix immich

--- config/homelab/homelab.nix
+++ config/homelab/homelab.nix
@@ -75,9 +75,9 @@
     };
     immich = {
       enable = true;
-      port = 2283;
-      mediaLocation = "/media/photos/";
-      host = "0.0.0.0";
+      # port = 2283;
+      # mediaLocation = "/media/photos/";
+      # host = "0.0.0.0";
     };
   };
 }
