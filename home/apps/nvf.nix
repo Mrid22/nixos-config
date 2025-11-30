4cb25a7abcab1b2cf1ae141160e8ed4f7e824e80
Mrid22 <mridulaga@outlook.com>
2025-11-30 21:27:42 +0100

    feat(nvf): added web dev support

--- home/apps/nvf.nix
+++ home/apps/nvf.nix
@@ -11,9 +11,19 @@
       tabstop = 2;
       shiftwidth = 2;
     };
+    treesitter = {
+      enable = true;
+      autoTagHtml = true;
+      context.enable = true;
+    };
     filetree.nvimTree = {
       enable = true;
       mappings.toggle = "<leader>e";
+    };
+    terminal.toggleterm = {
+      enable = true;
+      lazygit.enable = true;
+      mappings.open = "<leader>t";
     };
     autocomplete.blink-cmp = {
       enable = true;
@@ -25,8 +35,14 @@
     };
     languages = {
       enableFormat = true;
-      enableTreesitter = true;
       nix.enable = true;
+      html.enable = true;
+      css.enable = true;
+      tailwind.enable = true;
+      ts = {
+        enable = true;
+        extensions.ts-error-translator.enable = true;
+      };
     };
     theme = {
       enable = true;
