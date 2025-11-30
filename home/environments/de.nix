34477df385ef87fd7fb6dbf83319521950fc4fcc
Mrid22 <mridulaga@outlook.com>
2025-11-30 11:51:57 +0100

    hyprland: make home manager inherit flake

--- home/environments/de.nix
+++ home/environments/de.nix
@@ -49,6 +49,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland;
     settings = {
       exec-once = ["vicinae server"];
       bind = [
