01e5a889cbe7a0cb0f6dd6f515868317f4cb4b3e
Mrid22 <mridulaga@outlook.com>
2025-12-12 14:34:05 +0100

    did not work

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
@@ -83,7 +82,6 @@
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
-        inputs.nixos-hardware.nixosModules.lenovo-yoga-7-14ARH7-nvidia
       ];
     };
   };
