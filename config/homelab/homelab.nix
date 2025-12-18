0db0b3df42790e20c8e935ec5a1c97ef01af3264
Mrid22 <mridulaga@outlook.com>
2025-12-18 12:57:55 +0100

    typo

--- config/homelab/homelab.nix
+++ config/homelab/homelab.nix
@@ -5,15 +5,15 @@
   ...
 }: {
   imports = with inputs; [
-    sops-nix.nixosModules.default
+    #  sops-nix.nixosModules.default
     selfhostblocks.nixosModules.sops
   ];
 
-  sops = {
-    defaultSopsFile = ./secrets.yaml;
-    defaultSopsFormat = "yaml";
-    age.keyFile = "/home/mridula/.config/sops/age/keys.txt";
-  };
+  # sops = {
+  #   defaultSopsFile = ./secrets.yaml;
+  #   defaultSopsFormat = "yaml";
+  #   age.keyFile = "/home/mridula/.config/sops/age/keys.txt";
+  # };
 
   shb = {
     home-assistant = {
