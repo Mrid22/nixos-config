c47f942a3d5b3864f29dff1e4843a97689cca4e8
Mrid22 <mridulaga@outlook.com>
2025-12-19 23:15:34 +0100

    i no longer use zen browser

--- flake.nix
+++ flake.nix
@@ -20,14 +20,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
-
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
