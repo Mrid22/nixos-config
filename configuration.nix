f4e08d32c16373d0d93c69134365062a0b29d620
Mrid22 <mridulaga@outlook.com>
2026-01-03 20:42:29 +0100

    nvidia: prime

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,14 @@
       modesetting.enable = true;
       open = true;
       powerManagement.finegrained = true;
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+      };
     };
   };
 
