8716a7f1e5dbf91b0ff57d617033634b6498636d
Mrid22 <mridulaga@outlook.com>
2026-01-17 18:50:43 +0100

    no confirm window close

--- kitty.nix
+++ kitty.nix
@@ -7,6 +7,7 @@
           inherit pkgs;
           settings = {
             background_opacity = 0.5;
+            confirm_os_window_close = 0;
             themeFile = "Catppuccin-Mocha";
           };
         }
