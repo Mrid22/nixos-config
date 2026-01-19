14931eb0d4835984e27b8b99e786ddc32147838d
Mrid22 <mridulaga@outlook.com>
2026-01-19 18:03:41 +0100

    clean and gc redundant

--- configuration.nix
+++ configuration.nix
@@ -133,11 +133,6 @@ in {
 
   nix = {
     optimise.automatic = true;
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
 
     settings = {
       experimental-features = ["nix-command" "flakes"];
