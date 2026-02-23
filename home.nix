675cbc988eb5a2a7b98196fb2748ff037fd583ce
Mrid22 <mridulaga@outlook.com>
2026-02-23 21:51:23 +0200

    switch to zed for actual work

--- home.nix
+++ home.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   imports = with inputs; [
@@ -99,6 +100,68 @@
   };
   programs = {
     home-manager.enable = true;
+    zed-editor = {
+      enable = true;
+      extensions = ["nix" "javascript" "html" "python"];
+
+      userSettings = {
+        assistant.enabled = false;
+
+        node = {
+          path = lib.getExe pkgs.nodejs;
+          npm_path = lib.getExe' pkgs.nodejs "npm";
+        };
+
+        hour_format = "hour24";
+        auto_update = false;
+
+        terminal = {
+          alternate_scroll = "off";
+          blinking = "off";
+          copy_on_select = false;
+          dock = "bottom";
+          detect_venv = {
+            on = {
+              directories = [".env" "env" ".venv" "venv"];
+              activate_script = "default";
+            };
+          };
+
+          env.TERM = "kitty";
+
+          font_family = "FiraCode Nerd Font";
+          font_features = null;
+          font_size = null;
+          line_height = "comfortable";
+          option_as_meta = false;
+          button = false;
+          shell = "system";
+          # shell = {
+          #   program = "zsh";
+          # };
+          working_directory = "current_project_directory";
+        };
+
+        lsp = {
+          nix.binary.path_lookup = true;
+        };
+
+        vim_mode = true;
+
+        load_direnv = "shell_hook";
+        base_keymap = "VSCode";
+
+        theme = {
+          mode = "system";
+          light = "One Light";
+          dark = "One Dark";
+        };
+
+        show_whitespaces = "all";
+        ui_font_size = 16;
+        buffer_font_size = 16;
+      };
+    };
     waybar = {
       enable = true;
       systemd.enable = true;
