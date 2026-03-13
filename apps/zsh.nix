9f7ae7b10e5017a5a23d3ec89d552ad98c7d169c
Mrid22 <mridulaga@outlook.com>
2026-03-13 10:16:10 +0200

    zsh: completions

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -5,8 +5,22 @@
         inherit pkgs;
         settings = {
           autocd = true;
-          history.share = true;
           env.EDITOR = "$(which nvim)";
+          completion = {
+            colors = true;
+            caseInsensitive = true;
+            extraCompletions = true;
+          };
+          history = {
+            append = true;
+            share = true;
+            findNoDups = true;
+            ignoreAllDups = true;
+            ignoreDups = true;
+            ignoreSpace = true;
+            saveNoDups = true;
+          };
+          autoSuggestions.enable = true;
           integrations = {
             fzf.enable = true;
             starship.enable = true;
