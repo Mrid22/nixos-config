3eae1ea1a11e7e59ea8552f0fb5449fa8b8814e2
Mrid22 <mridulaga@outlook.com>
2026-03-16 12:48:54 +0200

    nvidia prime

--- modules/hosts/lenovo-laptop/configuration.nix
+++ modules/hosts/lenovo-laptop/configuration.nix
@@ -18,6 +18,18 @@
     hardware.nvidia = {
       open = true;
       modesetting.enable = true;
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
     };
     services.xserver.videoDrivers = ["nvidia"];
   };
