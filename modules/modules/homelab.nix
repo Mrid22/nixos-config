c347a2fd90ce0e09bf8bd822ccf732508644db2b
Mrid22 <mridulaga@outlook.com>
2026-03-21 22:41:21 +0200

    navidrome

--- modules/modules/homelab.nix
+++ modules/modules/homelab.nix
@@ -5,9 +5,9 @@
     lib,
     ...
   }: {
-    fileSystems."/media/" = {
+    fileSystems."/media" = {
       device = "/dev/sda1";
-      fsType = "exfat";
+      fsType = "ntfs";
       options = [
         "users" # Allows any user to mount and unmount
         "nofail" # Prevent system from failing if this drive doesn't mount
@@ -19,6 +19,10 @@
       enable = true;
       syncModels = true;
       loadModels = ["qwen3-coder-next:cloud"];
+    };
+    services.navidrome = {
+      enable = true;
+      settings.MusicFolder = "/media/music/";
     };
   };
 }
