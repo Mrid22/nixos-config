c3153364b0f4ef7ff4392f5c573b065191873f68
Mrid22 <mridulaga@outlook.com>
2026-03-22 19:58:30 +0200

    set histfile

--- modules/apps/zsh.nix
+++ modules/apps/zsh.nix
@@ -10,6 +10,7 @@
         settings = {
           autocd = true;
           env = {
+            HIST_FILE = "~/.zsh_history";
             EDITOR = "$(which nvim)";
             PATH = "/nix/store/bqz2sllks58anmkms91fynijx1xvvi8a-fzf-0.70.0/bin:/nix/store/y2jp6fwbsbfvqd4clr4jcx7455ainfll-zoxide-0.9.9/bin:/nix/store/dw1g75jyd5wc5q6hp5ss8sggqsryi1f6-starship/bin:/nix/store/01xjwa6xf1qcc0g1z806gh794k6q14vz-pay-respects-0.7.12/bin:/nix/store/ymjfzdhyy8lqr9bkyb3akljlk30g2jc6-nix-zsh-completions-0.5.1-unstable-2025-12-12/bin:/nix/store/b2bp7j8qzbfqzm5d2gmdvcb5lb6ghjwq-zsh-completions-0.35.0/bin:$PATH";
           };
