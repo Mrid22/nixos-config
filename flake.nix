e3ec903db65da488f395b5ee070fd2c84814138f
Mrid22 <mridulaga@outlook.com>
2026-03-26 20:51:12 +0200

    init noctalia

--- flake.nix
+++ flake.nix
@@ -41,6 +41,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/common/configuration.nix
+++ modules/common/configuration.nix
@@ -13,6 +13,8 @@
       experimental-features = ["nix-command" "flakes"];
       substituters = ["https://hyprland.cachix.org" "https://vicinae.cachix.org"];
       trusted-substituters = ["https://hyprland.cachix.org"];
+      extra-substituters = ["https://noctalia.cachix.org"];
+      extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
@@ -32,6 +34,8 @@
     networking.networkmanager.enable = true;
     services = {
       udisks2.enable = true;
+      upower.enable = true;
+      tuned.enable = true;
       playerctld.enable = true;
       gnome.gnome-keyring.enable = true;
 
