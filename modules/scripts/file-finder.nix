b67725ba888469ff8172801d2aacc51010b2d5d1
Mrid22 <mridulaga@outlook.com>
2026-04-16 21:06:33 +0300

    typo

--- modules/scripts/file-finder.nix
+++ modules/scripts/file-finder.nix
@@ -5,7 +5,7 @@
     packages.file-finder = pkgs.writeShellScriptBin "file-finder" ''
       ff() {
         local selected
-        selected=$(${pkgs.fzf}/bin/fzf --preview="${pkgs.bat}/bin/bat --color=always {}") --hidden --ignore .cache || return
+        selected=$(${pkgs.fzf}/bin/fzf --preview="${pkgs.bat}/bin/bat --color=always {}") --hidden --ignore *.cache* || return
         case "$(${pkgs.file}/bin/file --brief --mime-type "$selected")" in
           text/*|application/x-empty|inode/x-empty)
             ${wrappedpkgs.default}/bin/nvim "$selected"
