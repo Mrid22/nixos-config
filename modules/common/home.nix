b95e943d11a3943eda7318dcaf77a281678a4302
Mrid22 <mridulaga@outlook.com>
2026-05-23 11:19:00 +0300

    remove home manager hyprland

--- modules/common/home.nix
+++ modules/common/home.nix
@@ -15,28 +15,6 @@
       stateVersion = "25.11";
     };
     services = {
-      hypridle = {
-        enable = true;
-        settings = {
-          general = {
-            after_sleep_cmd = "hyprctl dispatch dpms on";
-            ignore_dbus_inhibit = false;
-            lock_cmd = "hyprlock";
-          };
-
-          listener = [
-            {
-              timeout = 180;
-              on-timeout = "hyprlock";
-            }
-            {
-              timeout = 300;
-              on-timeout = "hyprctl dispatch dpms off";
-              on-resume = "hyprctl dispatch dpms on";
-            }
-          ];
-        };
-      };
       vicinae = {
         enable = true;
         systemd = {
