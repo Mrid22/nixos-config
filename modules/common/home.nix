55d8f60351d9f0f01181ebf5e9de6f7dae228c51
Mrid22 <mridulaga@outlook.com>
2026-05-25 10:20:15 +0300

    fix vscode fhs

--- modules/common/home.nix
+++ modules/common/home.nix
@@ -61,7 +61,7 @@
       };
       vscode = {
         enable = true;
-        package = pkgs.vscode-fhs;
+        package = pkgs.vscode.fhs;
         profiles.default.extensions = with pkgs; [
           vscode-extensions.visualstudiotoolsforunity.vstuc
           vscode-extensions.vscodevim.vim
