bb2c3f39acd247db5b9905db5a6bcd920fd0f419
Mrid22 <mridulaga@outlook.com>
2026-06-24 11:41:36 +0300

    homelab specialization

--- modules/common/homelab.nix
+++ modules/common/homelab.nix
@@ -14,113 +14,115 @@
     environment.systemPackages = with pkgs; [
       cloudflared
     ];
-
-    fileSystems."/media" = {
-      device = "/dev/sda1";
-      fsType = "ntfs";
-      options = [
-        "users" # Allows any user to mount and unmount
-        "nofail" # Prevent system from failing if this drive doesn't mount
-        "exec" # Permit execution of binaries and other executable files
-      ];
-    };
-
-    virtualisation.oci-containers = {
-      backend = "podman";
-      containers = {
-        homeassistant = {
-          volumes = ["home-assistant:/config"];
-          environment.TZ = "Asia/Nicosia";
-          # Note: The image will not be updated on rebuilds, unless the version label changes
-          image = "ghcr.io/home-assistant/home-assistant:latest";
-          extraOptions = [
-            # Use the host network namespace for all sockets
-            "--network=host"
-          ];
-        };
+    specialisation.homelab = {
+      # Mount the drive
+      fileSystems."/media" = {
+        device = "/dev/sda1";
+        fsType = "ntfs";
+        options = [
+          "users" # Allows any user to mount and unmount
+          "nofail" # Prevent system from failing if this drive doesn't mount
+          "exec" # Permit execution of binaries and other executable files
+        ];
       };
-    };
 
-    services = {
-      #Arr
-      declarative-jellyfin = {
-        enable = true;
-        branding.customCss = ''@import url("https://cdn.jsdelivr.net/gh/lscambo13/ElegantFin@main/Theme/ElegantFin-jellyfin-theme-build-latest-minified.css");'';
-        system = {
-          pluginRepositories = [
-            {
-              content = {
-                Name = "Jellyfin Stable";
-                Url = "https://repo.jellyfin.org/files/plugin/manifest.json";
-              };
-              tag = "RepositoryInfo"; # Needed to generate the correct XML
-            }
-
-            {
-              content = {
-                Name = "Intro Skipper";
-                Url = "https://intro-skipper.org/manifest.json";
-              };
-              tag = "RepositoryInfo"; # Needed to generate the correct XML
-            }
-            {
-              content = {
-                Name = "Apple Music Metadata";
-                Url = "https://repo.xkrivo.net/jellyfin/manifest.json";
-              };
-              tag = "RepositoryInfo"; # Needed to generate the correct XML
-            }
-            {
-              content = {
-                Name = "IAmParadox";
-                Url = "https://www.iamparadox.dev/jellyfin/plugins/manifest.json";
-              };
-              tag = "RepositoryInfo"; # Needed to generate the correct XML
-            }
-            {
-              content = {
-                Name = "Jellyfin Enhanced";
-                Url = "https://raw.githubusercontent.com/n00bcodr/jellyfin-plugins/main/10.11/manifest.json";
-              };
-              tag = "RepositoryInfo"; # Needed to generate the correct XML
-            }
-          ];
-          serverName = "Mridjelly";
-          trickplayOptions = {
-            enableHwAcceleration = true;
-            enableHwEncoding = true;
+      virtualisation.oci-containers = {
+        backend = "podman";
+        containers = {
+          homeassistant = {
+            volumes = ["home-assistant:/config"];
+            environment.TZ = "Asia/Nicosia";
+            # Note: The image will not be updated on rebuilds, unless the version label changes
+            image = "ghcr.io/home-assistant/home-assistant:latest";
+            extraOptions = [
+              # Use the host network namespace for all sockets
+              "--network=host"
+            ];
           };
         };
       };
-      sonarr.enable = true;
-      prowlarr.enable = true;
-      radarr.enable = true;
-      bazarr.enable = true;
-      flaresolverr.enable = true;
-      seerr.enable = true;
 
-      transmission = {
-        enable = true;
-        openRPCPort = true;
-        settings = {
-          download-dir = "/media/downloads/complete/";
-          incomplete-dir = "/media/downloads/incomplete/";
-          incomplete-dir-enabled = true;
+      services = {
+        #Arr
+        declarative-jellyfin = {
+          enable = true;
+          branding.customCss = ''@import url("https://cdn.jsdelivr.net/gh/lscambo13/ElegantFin@main/Theme/ElegantFin-jellyfin-theme-build-latest-minified.css");'';
+          system = {
+            pluginRepositories = [
+              {
+                content = {
+                  Name = "Jellyfin Stable";
+                  Url = "https://repo.jellyfin.org/files/plugin/manifest.json";
+                };
+                tag = "RepositoryInfo"; # Needed to generate the correct XML
+              }
+
+              {
+                content = {
+                  Name = "Intro Skipper";
+                  Url = "https://intro-skipper.org/manifest.json";
+                };
+                tag = "RepositoryInfo"; # Needed to generate the correct XML
+              }
+              {
+                content = {
+                  Name = "Apple Music Metadata";
+                  Url = "https://repo.xkrivo.net/jellyfin/manifest.json";
+                };
+                tag = "RepositoryInfo"; # Needed to generate the correct XML
+              }
+              {
+                content = {
+                  Name = "IAmParadox";
+                  Url = "https://www.iamparadox.dev/jellyfin/plugins/manifest.json";
+                };
+                tag = "RepositoryInfo"; # Needed to generate the correct XML
+              }
+              {
+                content = {
+                  Name = "Jellyfin Enhanced";
+                  Url = "https://raw.githubusercontent.com/n00bcodr/jellyfin-plugins/main/10.11/manifest.json";
+                };
+                tag = "RepositoryInfo"; # Needed to generate the correct XML
+              }
+            ];
+            serverName = "Mridjelly";
+            trickplayOptions = {
+              enableHwAcceleration = true;
+              enableHwEncoding = true;
+            };
+          };
         };
-      };
+        sonarr.enable = true;
+        prowlarr.enable = true;
+        radarr.enable = true;
+        bazarr.enable = true;
+        flaresolverr.enable = true;
+        seerr.enable = true;
 
-      immich.enable = true;
+        transmission = {
+          enable = true;
+          openRPCPort = true;
+          settings = {
+            download-dir = "/media/downloads/complete/";
+            incomplete-dir = "/media/downloads/incomplete/";
+            incomplete-dir-enabled = true;
+          };
+        };
 
-      cloudflared = {
-        enable = true;
+        immich.enable = true;
 
-        tunnels."${tunnel-uuid}" = {
-          credentialsFile = "/etc/cloudflared/${tunnel-uuid}.json";
-          default = "http_status:404";
-          ingress = {
-            "jf.shmanju.org" = "http://localhost:8096";
-            "js.shmanju.org" = "http://localhost:5055";
-            "pg.shmanju.org" = "http://localhost:2283";
+        cloudflared = {
+          enable = true;
+
+          tunnels."${tunnel-uuid}" = {
+            credentialsFile = "/etc/cloudflared/${tunnel-uuid}.json";
+            default = "http_status:404";
+            ingress = {
+              "jf.shmanju.org" = "http://localhost:8096";
+              "js.shmanju.org" = "http://localhost:5055";
+              "pg.shmanju.org" = "http://localhost:2283";
+            };
           };
         };
       };
