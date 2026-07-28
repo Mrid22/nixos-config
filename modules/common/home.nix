d98a53ca0f4866ce732457aecf9722e2e240677f
Mrid22 <mridulaga@outlook.com>
2026-07-28 13:12:32 +0300

    this is actually the best way, lua syntax is too complex to nixify

--- modules/common/home.nix
+++ modules/common/home.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.home-manager.flakeModules.home-manager
   ];
@@ -294,99 +298,7 @@
     wayland.windowManager.hyprland = {
       enable = true;
       configType = "lua";
-      settings = {
-        bindel = [
-          ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-          ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-          ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-          ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
-          ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
-        ];
-        bindl = [
-          ", XF86AudioNext, exec, playerctl next"
-          ", XF86AudioPause, exec, playerctl play-pause"
-          ", XF86AudioPlay, exec, playerctl play-pause "
-          ", XF86AudioPrev, exec, playerctl previous   "
-        ];
-        bindm = [
-          "ALT, mouse:272, movewindow"
-          "ALT, mouse:273, resizewindow"
-        ];
-        bind = [
-          "ALT,Q,exec,kitty zsh"
-          "ALT,SPACE,exec,vicinae toggle"
-          "ALT,E,exec,kitty file-finder"
-          "ALT,F,exec,vicinae vicinae://launch/applications/zen-twilight"
-          "ALT SHIFT,S,exec,hyprshot -m region"
-          "ALT SHIFT,V,exec,vicinae vicinae://launch/clipboard/history"
-
-          "ALT,H,movefocus,l"
-          "ALT,J,movefocus,d"
-          "ALT,K,movefocus,u"
-          "ALT,L,movefocus,r"
-          "ALT,C,killactive"
-
-          "ALT,1,workspace,1"
-          "ALT,2,workspace,2"
-          "ALT,3,workspace,3"
-          "ALT,4,workspace,4"
-          "ALT,5,workspace,5"
-          "ALT,6,workspace,6"
-          "ALT,7,workspace,7"
-          "ALT,8,workspace,8"
-          "ALT,9,workspace,9"
-          "ALT,0,workspace,10"
-
-          "ALT SHIFT,1,movetoworkspace,1"
-          "ALT SHIFT,2,movetoworkspace,2"
-          "ALT SHIFT,3,movetoworkspace,3"
-          "ALT SHIFT,4,movetoworkspace,4"
-          "ALT SHIFT,5,movetoworkspace,5"
-          "ALT SHIFT,6,movetoworkspace,6"
-          "ALT SHIFT,7,movetoworkspace,7"
-          "ALT SHIFT,8,movetoworkspace,8"
-          "ALT SHIFT,9,movetoworkspace,9"
-          "ALT SHIFT,0,movetoworkspace,10"
-        ];
-        animations = {
-          enabled = "yes, please :)";
-
-          bezier = [
-            "easeOutQuint,0.23,1,0.32,1"
-            "easeInOutCubic,0.65,0.05,0.36,1"
-            "linear,0,0,1,1"
-            "almostLinear,0.5,0.5,0.75,1.0"
-            "quick,0.15,0,0.1,1"
-          ];
-
-          animation = [
-            "global, 1, 10, default"
-            "border, 1, 5.39, easeOutQuint"
-            "windows, 1, 4.79, easeOutQuint"
-            "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-            "windowsOut, 1, 1.49, linear, popin 87%"
-            "fadeIn, 1, 1.73, almostLinear"
-            "fadeOut, 1, 1.46, almostLinear"
-            "fade, 1, 3.03, quick"
-            "layers, 1, 3.81, easeOutQuint"
-            "layersIn, 1, 4, easeOutQuint, fade"
-            "layersOut, 1, 1.5, linear, fade"
-            "fadeLayersIn, 1, 1.79, almostLinear"
-            "fadeLayersOut, 1, 1.39, almostLinear"
-            "workspaces, 1, 1.94, almostLinear, slide"
-            "workspacesIn, 1, 1.21, almostLinear, slide"
-            "workspacesOut, 1, 1.94, almostLinear, slide"
-          ];
-        };
-        input.touchpad.natural_scroll = true;
-        general = {
-          gaps_in = 5;
-          gaps_out = 5;
-          border_size = 0;
-        };
-        decoration.rounding = 10;
-      };
+      extraConfig = builtins.readFile ./hyprland.lua;
     };
   };
 }
