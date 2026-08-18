e9da607ddb41ee87f9528cca8c2e493354d6d23e
Mrid22 <mridulaga@outlook.com>
2026-08-18 08:24:29 +0300

    had to reinstall

--- modules/hosts/lenovo-laptop/hardware-configuration.nix
+++ modules/hosts/lenovo-laptop/hardware-configuration.nix
@@ -1,31 +1,41 @@
-{ config, lib, pkgs, modulesPath, ... }: { flake.nixosModules.lenovo-laptop-hardware-config = {config,lib,modulesPath,...}:
-
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  flake.nixosModules.lenovo-laptop-hardware-config = {
+    config,
+    lib,
+    modulesPath,
+    ...
+  }: {
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "uas" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "uas" "sd_mod" "rtsx_pci_sdmmc"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1e42bddc-25cd-4b56-9957-da0664acf1ed";
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/e4571473-828f-4c11-93a9-7b9678f29b00";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/78BD-E3EC";
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/E8B2-63AB";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = ["fmask=0022" "dmask=0022"];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/6a3332f9-f183-4972-8f2e-055887eecb3d"; }
+    swapDevices = [
+      {device = "/dev/disk/by-uuid/d18fd6df-7399-4340-9591-2df2d958719c";}
     ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-};
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
