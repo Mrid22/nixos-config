7c4fff2c37b0c045261dce9561fa634c9686a82b
Mrid22 <mridulaga@outlook.com>
2026-08-31 14:54:44 +0300

    no need tailscale

--- modules/common/homelab.nix
+++ modules/common/homelab.nix
@@ -43,7 +43,6 @@
       };
 
       services = {
-        tailscale.enable = true;
         declarative-jellyfin = {
           enable = true;
           branding.customCss = ''@import url("https://cdn.jsdelivr.net/gh/lscambo13/ElegantFin@main/Theme/ElegantFin-jellyfin-theme-build-latest-minified.css");'';
